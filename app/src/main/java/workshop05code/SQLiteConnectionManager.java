06d22d366a6f91ecad644a0df139cf0a57f4b0fa
Jasonxiao1990 <68939686+Jasonxiao1990@users.noreply.github.com>
2023-03-20 12:12:44 +1100

    Update SQLiteConnectionManager.java

--- app/src/main/java/workshop05code/SQLiteConnectionManager.java
+++ app/src/main/java/workshop05code/SQLiteConnectionManager.java
@@ -145,10 +145,10 @@ public class SQLiteConnectionManager {
      * @return true if guess exists in the database, false otherwise
      */
     public boolean isValidWord(String guess) {
-        String sql = "SELECT count(id) as total FROM validWords WHERE word like'" + guess + "';";
+        String sql1 = "SELECT count(id) as total FROM validWords WHERE word like'" + guess + "';";
 
         try (Connection conn = DriverManager.getConnection(databaseURL);
-                PreparedStatement stmt = conn.prepareStatement(sql)) {
+                PreparedStatement stmt = conn.prepareStatement(sql1)) {
 
             ResultSet resultRows = stmt.executeQuery();
             if (resultRows.next()) {
